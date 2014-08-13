18cce5235e2310442430432010d860f119a55f8a
Wojciech Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-08-13 19:08:43 +0200

    minor fixes and frontend polishing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
+before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :remember_me) }
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
+  end
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
